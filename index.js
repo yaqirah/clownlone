03d8d457b0c94e65fcd7680373b50a52c57621b0
yaqirah <yaqirahrice@gmail.com>
2021-09-29 23:52:30 -0400

    real clown
    aaaa

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
-// Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+require('dotenv').load();
+const Discord = require("discord.js");
+const client = new Discord.Client({autoReconnect: true});
 
-// Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+var name = "Beta";
+var action = "coding";
+var keyword = "clown";
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+// emotes
+var clown = "<:looseclown:892945490730184745>";
+var shammy_clown = "<:shammyclown:892945388217192489>";
+var allowed = "<:allowed:803132424400928769>";
+var not_allowed = "<:notallowed:803156238673248297>";
+
+//when bot is activated
+client.on("ready", () => {
+	client.user.setGame(action)
+	console.log(keyword + "!");
 });
 
-// Login to Discord with your client's token
-client.login(token);
+//new message sent
+client.on("message", async message => {
+	if(message.author.bot){return;}
+	if (!message.guild){ return};
+	
+	if(message.content.includes(keyword)){
+		if(message.content.charAt(message.content.length - 1) == "?"){ // ends with ?
+			if(Math.round(Math.random()) == 0){
+				message.channel.send(allowed);
+			}else{
+				message.channel.send(not_allowed);
+			}
+		}else{
+			message.channel.send(clown);
+		}
+	}else if(message.content.includes("shammy")){
+		message.channel.send(shammy_clown);
+	}
+});
+client.login(process.env.TOKEN)
