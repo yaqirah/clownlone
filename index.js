08972d8a7022715aabea268a8c580261830ba707
yaqirah <yaqirahrice@gmail.com>
2021-10-22 17:05:35 -0400

    schlorp

--- index.js
+++ index.js
@@ -23,13 +23,14 @@ var allowed = "<:allowed:893221037053968404>";
 var not_allowed = "<:notallowed:893221037465010196>";
 
 //songs
-var sendintheclowns = ["Isn't it rich?", "Are we a pair?", "Me here at last on the ground", "You in mid-air", "Send in the clowns", "Isn't it bliss?", "Don't you approve?", "One who keeps tearing around", "One who can't move", "Where are the clowns?", "Send in the clowns", "Just when I stopped", "Opening doors", "Finally knowing the one that I wanted was yours", "Making my entrance again with my usual flair", "Sure of my lines", "Noone is there", "Don't you love a farce?", "My fault, I fear", "I thought that you'd want what I want", "Sorry my dear", "But where are the clowns?", "Send in the clowns", "Don't bother", "They're here"]
-var sendintheclowns2 =[] //punctuation free version
+var sendintheclowns = ["Isn't it rich?", "Are we a pair?", "Me here at last on the ground", "You in mid-air", "Send in the clowns", "Isn't it bliss?", "Don't you approve?", "One who keeps tearing around", "One who can't move", "Where are the clowns?", "Send in the clowns", "Just when I stopped", "Opening doors", "Finally knowing the one that I wanted was yours", "Making my entrance again with my usual flair", "Sure of my lines", "Noone is there", "Don't you love a farce?", "My fault, I fear", "I thought that you'd want what I want", "Sorry my dear", "But where are the clowns?", "Send in the clowns", "Don't bother", "They're here"];
+var sendintheclowns2 =[]; //punctuation free version
 
 //misc
 dmMessages = ["UNAUTHORIZED USER: ENTERING *'Kill Mode'.*", "Subscribe to Clown: Premium for private DM consultations! only $24.99 USD a month!", "honk!!!", ":o)", "\*depressed honk\*", "Please leave me alone :/", "i'm on a bus to your house right now and i'm only 10 minutes away. watch your back bozo.", "I heard the magician and the knife thrower are an item now. you didn't hear it from me though :shushing_face: :flushed:", ":clown:", "please stop", "my mom said i'm not allowed to talk to strangers, sorry. take it up with her: <@416723258449330186>", "BOO!!!! did i scare u :o) hehe happy hallowe ween", "you will on july 21st 2036 in a ditch in memphis tennessee. sorry.", "do you think you're funny :/", "oh a wise guy huh? whhy i oughta.... \*pushes up sleeves revealing extremely skinny arms\*"]
 rateMessages = [" is 0% clowning. *yaaaawn*.", " is 0% clowning. Are you even trying???", " is 1% clowning. that's pitiful :/", " is 10% clowning. hey, go back to business school bozo!!", " is 15% clowning. uhh, amateur hour?? am i right folks?", " is 20% clowning. i think we should see other people :/", " is 25% clowning. don't quit your day job, buddy.", " is 30% clowning. keep at it and maybe you'll be on my level... in a hundred years!", " is 40% clowning. still more on the side of wiseguy than funnyman, but i see the potential.", " is 50% clowning. hey, maybe you do have a funnybone in you, kid.", " is 60% clowning. hee hee hoo hoo!!", " is 70% clowning. now that's a laugh riot.", " is 75% clowning. You're on your way to the top! the big top that is!!", " is 80% clowning. funny, funny stuff!", " is 85% clowning. Stop, stop! you're killin me kid!", " is 90% clowning. yeowza!", " is 95% clowning. do you think you're funny :/ WELL YOU ARE!!!!!!", " is 99% clowning...... impossible..... these readings..!!!", " is 100% clowning. you're not just a clown, you're the entire circus!!!", " is 101% clowning. Your power levels are incredible! I'm scared!!!!!!!!!! mommy!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"];
 statusList = ["Entrance of the Gladiators", "clowning", "with your heart","doing a little dance", ":o)", "🤡", "thinking of a prank", "pie throwing", "scheming", "doing shammy's taxes", "chewing", "lunch break", "funny", "i can do anything", "metamorphosis", "certified FunnyMan", "charging beam attack", "activating special move", "pretending to sleep", "watching you sleep", "wondering who my father is"];
+schlorpList = ["never schlorping", "always schlorping"];
 
 //when bot is activated
 client.on("ready", () => {
@@ -110,13 +111,14 @@ client.on("message", async message => {
 		client.user.setGame("watching " + message.author.username.toLowerCase() + " sleep");
 	}
 	
+	if(message.content.toLowerCase().startsWith("schlorpscale")){
+		var name = get_name(message, "schlorpscale");
+		message.channel.send(name + " is " + schlorpList[Math.floor(Math.random()*schlorpList.length)]);
+		return;
+	}
+	
 	if (message.content.toLowerCase().startsWith("clownrate")) {
-		var name = ""
-		if (message.content.length>=10) {
-			name = message.content.substring(10);
-		} else {
-			name = message.author.username;
-		}
+		var name = get_name(message, "clownrate");
 		message.channel.send(name + rateMessages[Math.floor(Math.random()*rateMessages.length)]);
 		return;
 	}
@@ -205,6 +207,20 @@ client.on("message", async message => {
 		send_clown(message);
 	}
 });
+
+function get_name(message, keyword){
+	var name = "";
+	if (message.content.length>=keyword.length+1) {
+		name = message.content.substring(keyword.length+1);
+	} else {
+		name = message.author.username;
+	}
+	return capitalize_first_letter(name);
+}
+
+function capitalize_first_letter(string){
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 function send_clown(message){
 	message.react(simple_emote(clown));
